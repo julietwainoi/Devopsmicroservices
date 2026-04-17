c3644d40d070deb176cd20bb1f563bd87f3a3942
julietwainoi <juliewainoi@gmail.com>
2026-04-17 08:46:43 +0300

    changed usestateset form PVC

--- order-service/db.py
+++ order-service/db.py
@@ -1,14 +1,13 @@
+import os
 import psycopg2
-from psycopg2.extras import Json
 
 def get_connection():
     conn = psycopg2.connect(
-        host="order-db",
-        database="orders_user",
-        user="orders_user",
-        password="orders_pass"
+        host=os.getenv("DB_HOST", "order-db"),
+        database=os.getenv("DB_NAME", "orders_user"),
+        user=os.getenv("DB_USER", "orders_user"),
+        password=os.getenv("DB_PASS", "orders_pass")
     )
-    # Set the default search_path
     with conn.cursor() as cur:
         cur.execute("SET search_path TO orders, public;")
     return conn
