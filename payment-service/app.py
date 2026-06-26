09881ee410b2b0410bf839da3474c86f7b17b260
julietwainoi <juliewainoi@gmail.com>
2026-06-26 16:50:10 +0300

    added thestrick slash false on the payment-services

--- payment-service/app.py
+++ payment-service/app.py
@@ -2,7 +2,7 @@ from flask import Flask, request, jsonify
 
 app = Flask(__name__)
 
-@app.route("/health")
+@app.route("/health", strict_slashes=False)
 def health():
     return {"status": "healthy"}, 200
 
